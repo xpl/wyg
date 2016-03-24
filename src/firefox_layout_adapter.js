cfb9b88a7d604d27efba1d80bdc766b38a01974b
xpl <rocket.mind@gmail.com>
2016-03-24 03:38:45 +0300

    tabs → spaces

--- src/firefox_layout_adapter.js
+++ src/firefox_layout_adapter.js
@@ -3,17 +3,17 @@ Wyg_FirefoxLayoutAdapter = !Platform.Firefox ? $trait () : $trait ({
 /*  Firefox has nasty resizing handles for absolute-positioned elements
     contained within a contenteditable. There's no way to disable them.
 
-	The workaround is to turns off absolute layout when dragging is not
-	active. This disables fancy paragraph sliding animations when typing
-	text, and should be considered as a temporary work-around.
+    The workaround is to turns off absolute layout when dragging is not
+    active. This disables fancy paragraph sliding animations when typing
+    text, and should be considered as a temporary work-around.
 
-	Will wait until Firefox implements grabber styling/disabling.
+    Will wait until Firefox implements grabber styling/disabling.
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
     domReady: function (dom) {
-    	dom.toggleAttribute ('static-layout', true) },
+        dom.toggleAttribute ('static-layout', true) },
 
     isDragging: function (yes) {
-    	this.domReady (function (dom) {
-        	dom.toggleAttribute ('static-layout', !yes) }) }
+        this.domReady (function (dom) {
+            dom.toggleAttribute ('static-layout', !yes) }) }
 })
