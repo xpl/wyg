c9fd29f6d27647b932abd18b60ec9b4c6da5b26d
xpl <rocket.mind@gmail.com>
2016-03-28 01:22:36 +0300

    tabs → spaces

--- demo.js
+++ demo.js
@@ -1,29 +1,29 @@
-/*	Run with:
+/*  Run with:
 
-		> node demo.js
+        > node demo.js
  */
 
 require ('useless')
 
 App = $singleton (Component, {
 
-	api: function () { return {
+    api: function () { return {
 
-		'/':
-			this.file ('./demo.html'),
+        '/':
+            this.file ('./demo.html'),
 
-		'api/upload': {
-			post: this.uploadImageTo ('./uploads') },
+        'api/upload': {
+            post: this.uploadImageTo ('./uploads') },
 
-		':file':
-			this.file ('./') } },
+        ':file':
+            this.file ('./') } },
 
-	$depends: [
-		require ('useless/server/supervisor'),
-		require ('useless/server/request'),
-		require ('useless/server/http'),
-		require ('useless/server/uploads') ],
+    $depends: [
+        require ('useless/server/supervisor'),
+        require ('useless/server/request'),
+        require ('useless/server/http'),
+        require ('useless/server/uploads') ],
 
-	init: function (then) {
-		then ()
-		log.green ('Example app is running at ', log.color.boldGreen, 'http://localhost:1333') } })
+    init: function (then) {
+        then ()
+        log.green ('Example app is running at ', log.color.boldGreen, 'http://localhost:1333') } })
