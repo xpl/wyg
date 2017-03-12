4f3f0f43b652dd6ea73c69932e55380e6ec11119
xpl <rocket.mind@gmail.com>
2017-03-12 04:41:41 +0300

    implemented inserting text paragraphs before and after images

--- src/inserting_paragraphs.js
+++ src/inserting_paragraphs.js
@@ -1,6 +1,5 @@
 Wyg_InsertingParagraphs = $trait ({
 
-
 	domReady (dom) {
 
 		dom.on ('click', e => {
@@ -10,13 +9,26 @@ Wyg_InsertingParagraphs = $trait ({
 				const hit = this.hitTest (Vec2.xy (e.offsetX, e.offsetY)) // analyze hit
 				const row = hit.row && hit.row[0]
 				
-				if (row.isDDRow && (hit.side_v === 'bottom') && !(row.nextSibling && row.nextSibling.isTextRow)) {
+				if (row.isDDRow && hit.side_v) {
 
-					//log.gg (true)
+					const hasNextTextRow = (row.nextSibling && row.nextSibling.isTextRow)
+					const hasPrevTextRow = (row.prevSibling && row.prevSibling.isTextRow)
 
+					if (!(hit.side_v === 'bottom' && hasNextTextRow) &&
+						!(hit.side_v === 'top'    && hasPrevTextRow)) {
+
+						this.insertPromptParagraph (hit)
+					}
 				}
-
 			}
 		})
-	}
+	},
+
+	insertPromptParagraph: $customCommand (function (hit) {
+
+		N ('p', N ('br'))
+				[{ bottom: 'insertMeAfter', top: 'insertMeBefore' }[hit.side_v]] (hit.row[0])
+				.moveCaret ()
+
+	})
 })
