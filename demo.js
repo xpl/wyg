1587c35a2f16112ed6c48918da9f3f3cef584d98
xpl <rocket.mind@gmail.com>
2017-07-05 12:13:16 +0300

    useless library update

--- demo.js
+++ demo.js
@@ -3,20 +3,43 @@
         > node demo.js
  */
 
-require ('useless')
+const _         = require ('useless')
+const { mkdir } = require ('useless/server/base/fs')
 
-App = $singleton (Component, {
+/*
+теперь в Useless можно HTTP routes определять просто как мемберы компонента, т.е. было так:
+
+```
+WygDemoApp = $singleton (Component, {
 
     api: function () { return {
 
-        '/':
-            this.file.$ ('./demo.html'),
+        ‘/’:
+            this.file.$ (‘./demo.html’),
 
-        'api/upload': {
-            post: this.uploadImageTo.$ ('./upload') },
+        ‘api/upload’: {
+            post: this.uploadImageTo.$ (‘./upload’) },
 
-        ':file':
-            this.file.$ ('./') } },
+        ‘:file’:
+            this.file.$ (‘./‘) } },
+
+ ....
+ */
+
+WygDemoApp = $singleton (Component, {
+
+    // api () { return {
+
+    //     '/':
+    //         this.file.$ ('./demo.html'),
+
+    //     'api/upload': {
+    //         post: this.uploadImageTo.$ ('./upload') },
+
+    //     ':file':
+    //         this.file.$ ('./')
+
+    // }},
 
     $depends: [
         
@@ -26,9 +49,20 @@ App = $singleton (Component, {
         require ('useless/server/source')
     ],
 
-    shouldRestartOnSourceChange: function (action, file, yes, no) {
-                                    if (file.contains (__dirname + '/upload')) no () },
+    '/': () => $this.file ('./demo.html'),
 
-    init: function () {
-            require ('useless/server/base/fs').mkdir ('upload').catch (() => {})
-            log.green ('Example app is running at ', log.color.boldGreen, 'http://localhost:1333') } })
+    '/api/upload': { post: () => $this.uploadImageAsJPEG ('./upload') },
+
+    '/:file': () => $this.file ('./'),
+
+    shouldRestartOnSourceChange (action, file, yes, no) {
+                                    if (file.contains (__dirname + '/upload')) {
+                                        no () } },
+
+    async init () {
+        
+        await mkdir ('upload').catch (() => {})
+
+        log.green ('Example app is running at ', log.color.boldGreen, 'http://localhost:1333')
+    }
+})
